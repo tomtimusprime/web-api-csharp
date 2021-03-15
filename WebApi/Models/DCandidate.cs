037414d86a6471810a01f269e525f53169e03aa6
Tom Black <tom.black06007@gmail.com>
2021-03-14 20:27:57 -0700

    * .gitignore: Started working on the backend in C#.
    * WebApi.sln:
    * Program.cs:
    * Startup.cs:
    * WebApi.csproj:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * DCandidate.cs:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Privacy.cshtml:
    * Error.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * DonationDBContext.cs:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.min.css:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ WebApi/Models/DCandidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class DCandidate
+    {
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string mobile { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+        public string bloodGroup { get; set; }
+        public string address { get; set; }
+
+    }
+}
